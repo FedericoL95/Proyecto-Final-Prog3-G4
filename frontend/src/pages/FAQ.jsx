23d6a4e99987e3d6bad3eb716eefafbefa6b715d
Gino <gmarigioli@gmail.com>
2026-07-16 22:40:09 -0300

    feat: añado perfil.jsx en pages y FAQ.html en public, modifico FAQ.jsx

--- frontend/src/pages/FAQ.jsx
+++ frontend/src/pages/FAQ.jsx
@@ -1,3 +1,4 @@
+import Card from "frontend/src/components/ui/Card"; // Podria utilizarse para que quede mejor con el resto de la pagina, pero se me ocurrio despues de que ya habia escribido todo
 export default function FAQ() {
   return (
     <div className="page">
