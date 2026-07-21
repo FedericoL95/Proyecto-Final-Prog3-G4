bf5ad55892196aea5dbe3896b9f8d90fd1870eb5
niko <nikolasr010605@gmail.com>
2026-07-21 06:30:05 +0000

    Cambios menores

--- backend/models/Libro.js
+++ backend/models/Libro.js
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-const { DataTypes } = require('sequelize');
-
-module.exports = (sequelize) => {
-  const Libro = sequelize.define('Libro', {
-    idLibro: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    titulo: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-        len: [2, 100]
-      }
-    },
-    autor: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    estado: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      },
-    puntuacion: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      validate: {
-        min: 1,
-        max: 5
-      }
-    },
-    reseña: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-      validate: {
-        len: [0, 500]
-      }
-    },
-    idGenero: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'generos',
-        key: 'idGenero'
-      }
-    }
-  }, {
-    tableName: 'libros',
-    timestamps: true
-   });
-  return Libro;
-=======
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
@@ -107,5 +51,4 @@ module.exports = (sequelize) => {
     timestamps: true
    });
   return Libro;
->>>>>>> 43d0b2abc4ab08f26f0c96e117887fc885e3c3a2
 };
