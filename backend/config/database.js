7ba4d5fc63986c01c285af91ac1ccfdd5c6bfc2c
niko <nikolasr010605@gmail.com>
2026-07-21 05:56:00 +0000

    feat: Modifico config y database para que funcione correctamente con Neon

--- backend/config/database.js
+++ backend/config/database.js
@@ -15,6 +15,12 @@ module.exports = {
       min: 0,
       acquire: 30000,
       idle: 10000
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
   },
   test: {
